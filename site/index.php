4e69cc3a9ff76e71e08ed561b9d3609eee608555
Kallum Panditharatna <kallum179@gmail.com>
2020-09-10 23:13:39 +0100

    Site structuring started

---
+++ site/index.php
@@ -0,0 +1,29 @@
+<div class="Login">
+  <form action="includes/zLogin.inc.php" method="POST">
+    <input class="username_txt" type="text" name="username" placeholder="Enter username">
+    <input class="password_txt" type="password" name="password" placeholder="Enter password">
+    <button class="login_btn" type="submit" name="login"> Login </button>
+  </form>
+</div>
+
+
+<div class="Friendslist">
+
+</div>
+
+<div class="RecentMessages">
+
+</div>
+
+
+<div class="ChatRoom">
+
+
+
+  <form action="index.php" method="POST">
+    <input class="messageEntry" type="text" name="messageEntry" placeholder="Type your message here">
+    <button class="messageSend" type="submit" name="messageSend"> Send </button>
+  </form>
+
+
+</div>
