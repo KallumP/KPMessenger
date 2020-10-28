7baf1cbce8d5e6efa790b5d8391355c117ad04d4
Kallum Panditharatna <kallum179@gmail.com>
2020-10-28 20:25:08 +0000

    Site can now pull messages from database, message table can now store a message

--- site/index.php
+++ site/index.php
@@ -13,69 +13,124 @@ session_start();
 </head>
 
 <body>
-  <header>
 
-    <?php include("includes/accountBanner.inc.php"); ?>
+  <div class=Container>
+    <header>
 
-    <div class="Actions">
-      <ul>
-        <li><a href="searchFriends.php">Friends</a></li>
-        <li><a href="searchAllUsers.php">Search all Users</a></li>
-      </ul>
-    </div>
+      <?php include("includes/accountBanner.inc.php"); ?>
 
-  </header>
+      <div class="Actions">
+        <ul>
+          <li><a href="searchFriends.php">Friends</a></li>
+          <li><a href="searchAllUsers.php">Search all Users</a></li>
+        </ul>
+      </div>
 
-  <div class="RecentMessages">
-    <h1>Recent Messages</h1>
-    <?php
-    for ($i = 0; $i < 9; $i++) {
+    </header>
 
-      echo "<div class='MessagePrev'>";
-      echo "<a href=index.php?ChatRoomID=" . $i . ">";
-      echo "<h1>" . $i . " Sender</h1>";
-      echo "<p>" . $i . " Preview</p>";
-      echo "</a>";
-      echo "</div>";
-    }
-    ?>
-  </div>
-
-  <div class="ChatRoom">
-
-    <div class="Messages">
+    <div class="RecentMessages">
+      <h1>Recent Messages</h1>
       <?php
+      for ($i = 0; $i < 9; $i++) {
 
-      for ($i = 0; $i < 1; $i++) {
-
-        $sent = rand(0, 1);
-
-        if ($sent == 1) {
-          echo "<div class='SentMessage Message'>";
-
-          echo "<h3>" . $i . " Sent</h3><br>";
-          echo "<p>" . $i . " Content</p>";
-
-          echo "</div>";
-        } else {
-          echo "<div class='RecievedMessage Message'>";
-
-          echo "<h3>" . $i . " Recieved</h3><br>";
-          echo "<p>" . $i . " Content</p>";
-
-          echo "</div>";
-        }
+        echo "<div class='MessagePrev'>";
+        echo "<a href=index.php?ChatRoomID=" . $i . ">";
+        echo "<h1>" . $i . " Sender</h1>";
+        echo "<p>" . $i . " Preview</p>";
+        echo "</a>";
+        echo "</div>";
       }
       ?>
     </div>
 
-    <div class="MessageInput">
-      <form action="index.php" method="POST">
-        <input class="messageEntry" type="text" name="messageEntry" placeholder="Type your message here">
-        <button class="messageSend" type="submit" name="messageSend"> Send </button>
-      </form>
+    <div class="ChatRoom">
+
+      <div class="Messages">
+        <?php
+
+        //checks if there was a chatroom that was selected
+        if (isset($_GET['ChatRoomID'])) {
+
+
+          $ChatroomID = mysqli_real_escape_string($conn, $_GET['ChatRoomID']);
+
+          //pulls the user's friend's ids
+          $sqlAllMessages =
+            "SELECT
+              message.Content AS 'MessageContent',
+              message.SenderID as 'SenderID'
+            FROM
+              message
+            WHERE
+              message.ChatRoomID = '$ChatroomID';";
+
+          $AllMessagesResult = mysqli_query($conn, $sqlAllMessages);
+          $AllMessagesResultCheck = mysqli_num_rows($AllMessagesResult);
+
+          //checks if there were any messages
+          if ($AllMessagesResultCheck > 0) {
+
+            //loops through all the messages
+
+            while ($messageRow = mysqli_fetch_assoc($AllMessagesResult)) {
+
+
+              $senderID = $messageRow['SenderID'];
+
+
+              //checks if the current message was yours
+              if ($senderID == $_SESSION['userID']) {
+
+                echo "<div class='SentMessage Message'>";
+
+                echo "<h3> Sent by you</h3><br>";
+                echo "<p>" .  $messageRow['MessageContent'] . "</p>";
+
+                echo "</div>";
+
+              } else {
+
+
+                $sqlGetSender =
+                  "SELECT
+                    _user.Username AS 'SenderName'
+                  FROM
+                    _user
+                  WHERE
+                    _user.ID = '$senderID';";
+
+                $GetSenderResult = mysqli_query($conn, $sqlGetSender);
+                $GetSenderResultCheck = mysqli_num_rows($GetSenderResult);
+
+
+                $senderName = "";
+                if ($GetSenderResultCheck > 0)
+                  $senderName = mysqli_fetch_assoc($GetSenderResult)['SenderName'];
+
+                else
+                  $senderName = "Unknown User";
+
+
+                echo "<div class='RecievedMessage Message'>";
+
+                echo "<h3>Sent by " . $senderName . "</h3><br>";
+                echo "<p>" .  $messageRow['MessageContent'] . "</p>";
+
+                echo "</div>";
+              }
+            }
+          }
+        }
+        ?>
+      </div>
+
+      <div class="MessageInput">
+        <form action="index.php" method="POST">
+          <input class="messageEntry" type="text" name="messageEntry" placeholder="Type your message here">
+          <button class="messageSend" type="submit" name="messageSend"> Send </button>
+        </form>
+      </div>
+
     </div>
-
   </div>
-
 </body>
