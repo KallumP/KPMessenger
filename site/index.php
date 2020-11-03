b83d79451ca3804139d711c3fd08fd1e23f57fba
Kallum Panditharatna <kallum179@gmail.com>
2020-11-03 10:54:10 +0000

    Chatbox sizing worked on, Chatname now shows up on the chatbox, recent chats should be in order

--- site/index.php
+++ site/index.php
@@ -65,7 +65,9 @@ session_start();
             chatroom
           LEFT JOIN connector ON chatroom.ID = connector.ChatroomID
           WHERE 
-            connector.UserID = '$UserID';";
+            connector.UserID = '$UserID'
+          ORDER BY 
+            chatroom.LastMessageTime DESC;";
 
       $RecentMessagesResult = mysqli_query($conn, $sqlGetRecentMessages);
 
@@ -119,7 +121,6 @@ session_start();
               //checks if there 
               if ($lastMessage != "") {
 
-
                 //checks if the message was more than 20 characters long and then saves the preview
                 if (strlen($lastMessage) > 20)
                   $messagePreview = substr($lastMessage, 0, 20);
@@ -131,8 +132,6 @@ session_start();
             }
           }
 
-
-
           //outputs the  chatroom
           echo "<div class='MessagePrev'>";
           echo "<a href=index.php?ChatRoomID=" . $recentMessageRow['ChatID'] . ">";
@@ -143,38 +142,51 @@ session_start();
         }
       }
 
-
       ?>
     </div>
 
     <div class="ChatRoom">
 
-      <div class="Messages">
-        <?php
+      <?php
 
-        //checks if there was a chatroom that was selected (from the url)
-        if (isset($_GET['ChatRoomID'])) {
+      //checks if there was a chatroom that was selected (from the url)
+      if (isset($_GET['ChatRoomID'])) {
 
+        $ChatroomID = mysqli_real_escape_string($conn, $_GET['ChatRoomID']);
+        $UserID = $_SESSION['userID'];
 
-          $ChatroomID = mysqli_real_escape_string($conn, $_GET['ChatRoomID']);
-          $UserID = $_SESSION['userID'];
-
-          //check if the user has access to this chatroom
-          $sqlUserConnector =
-            "SELECT 
+        //check if the user has access to this chatroom
+        $sqlUserConnector =
+          "SELECT 
               connector.ID
             FROM  
               connector
             WHERE
               connector.UserID = '$UserID';";
 
+        //if the user has access to this chat (the query returned a connector)
+        if (mysqli_num_rows(mysqli_query($conn, $sqlUserConnector))) {
 
-          if (mysqli_num_rows(mysqli_query($conn, $sqlUserConnector))) {
+          $sqlChatName =
+            "SELECT
+              chatroom.Name AS 'Name'
+            FROM
+              chatroom
+            WHERE
+              chatroom.ID = '$ChatroomID'";
 
+          $ChatNameResult = mysqli_query($conn, $sqlChatName);
 
-            //pulls the messages from this chatroom
-            $sqlAllMessages =
-              "SELECT
+          //checks if there were any messages
+          if (mysqli_num_rows($ChatNameResult) > 0) {
+            $chatname = mysqli_fetch_assoc($ChatNameResult)['Name'];
+            echo "<h1 class='ChatName'>" . $chatname .  "</h1>";
+          }
+          echo "<div class='Messages'>";
+
+          //pulls the messages from this chatroom
+          $sqlAllMessages =
+            "SELECT
               message.Content AS 'MessageContent',
               message.SenderID as 'SenderID'
             FROM
@@ -184,108 +196,87 @@ session_start();
             ORDER BY
               message.ID;";
 
-            $AllMessagesResult = mysqli_query($conn, $sqlAllMessages);
-            $AllMessagesResultCheck = mysqli_num_rows($AllMessagesResult);
+          $AllMessagesResult = mysqli_query($conn, $sqlAllMessages);
+          $AllMessagesResultCheck = mysqli_num_rows($AllMessagesResult);
 
-            //checks if there were any messages
-            if ($AllMessagesResultCheck > 0) {
+          //checks if there were any messages
+          if ($AllMessagesResultCheck > 0) {
 
-              //loops through all the messages
+            //loops through all the messages
+            while ($messageRow = mysqli_fetch_assoc($AllMessagesResult)) {
 
-              while ($messageRow = mysqli_fetch_assoc($AllMessagesResult)) {
+              $senderID = $messageRow['SenderID'];
+              $message = wordwrap($messageRow['MessageContent'], 70, "<br>");
 
+              //checks if the current message was yours
+              if ($senderID == $_SESSION['userID']) {
 
-                $senderID = $messageRow['SenderID'];
+                echo "<div class='SentMessage Message'>";
 
-                $message = wordwrap($messageRow['MessageContent'], 70, "<br>");
+                echo "<p>" .  $message . "</p>";
+                echo "<h3> Sent by you</h3>";
 
-
-                //checks if the current message was yours
-                if ($senderID == $_SESSION['userID']) {
-
-                  echo "<div class='SentMessage Message'>";
-
-                  echo "<p>" .  $message . "</p>";
-                  echo "<h3> Sent by you</h3>";
-
-                  echo "</div>";
-                } else {
+                echo "</div>";
+              } else {
 
 
-                  $sqlGetSender =
-                    "SELECT
+                $sqlGetSender =
+                  "SELECT
                     _user.Username AS 'SenderName'
                   FROM
                     _user
                   WHERE
                     _user.ID = '$senderID';";
 
-                  $GetSenderResult = mysqli_query($conn, $sqlGetSender);
-                  $GetSenderResultCheck = mysqli_num_rows($GetSenderResult);
+                $GetSenderResult = mysqli_query($conn, $sqlGetSender);
+                $GetSenderResultCheck = mysqli_num_rows($GetSenderResult);
 
+                $senderName = "";
+                if ($GetSenderResultCheck > 0)
+                  $senderName = mysqli_fetch_assoc($GetSenderResult)['SenderName'];
 
-                  $senderName = "";
-                  if ($GetSenderResultCheck > 0)
-                    $senderName = mysqli_fetch_assoc($GetSenderResult)['SenderName'];
+                else
+                  $senderName = "Unknown User";
 
-                  else
-                    $senderName = "Unknown User";
+                echo "<div class='RecievedMessage Message'>";
 
+                echo "<p>" .  $message . "</p>";
+                echo "<h3>Sent by " . $senderName . "</h3>";
 
-                  echo "<div class='RecievedMessage Message'>";
-
-                  echo "<p>" .  $message . "</p>";
-                  echo "<h3>Sent by " . $senderName . "</h3>";
-
-                  echo "</div>";
-                }
+                echo "</div>";
               }
-            } else {
-
-              //there were no messages in the chat
-
-              echo "<h2>Looks like there are no messages in this chat.<br>Why not initiate and send one yourself</h3><br><br>";
             }
           } else {
-            echo "<h2>You don't have access to this chat</h3>";
+
+            //there were no messages in the chat
+
+            echo "<h2>Looks like there are no messages in this chat.<br>Why not initiate and send one yourself</h3><br><br>";
           }
-
-        ?>
-          <!-- <div class="MessageInput">
-
-            <?php
-            //generates the url to send the message with
-            $SendMessageTo = "includes/zSendMessage.php?chatRoomID=" . mysqli_real_escape_string($conn, $_GET['ChatRoomID']);
-            echo "<form action='$SendMessageTo'  method='POST'>"
-            ?>
-
-            <input class="messageEntry BorderInputs" type="text" name="messageEntry" placeholder="Type your message here">
-            <button class="messageSend BorderInputs" type="submit" name="messageSend"> Send </button>
-            </form>
-          </div> -->
-        <?php
+        } else {
+          echo "<h2>You don't have access to this chat</h3>";
         }
-        ?>
-      </div>
-
-      <div class="MessageInput">
-
-        <?php
-        if (isset($_GET['ChatRoomID'])) {
-          //generates the url to send the message with
-          $SendMessageTo = "includes/zSendMessage.php?chatRoomID=" . mysqli_real_escape_string($conn, $_GET['ChatRoomID']);
-          echo "<form action='$SendMessageTo'  method='POST'>"
-        ?>
-
-          <input class="messageEntry BorderInputs" type="text" name="messageEntry" placeholder="Type your message here">
-          <button class="messageSend BorderInputs" type="submit" name="messageSend"> Send </button>
-          </form>
-        <?php
-        }
-        ?>
-      </div>
-
-
+      }
+      ?>
     </div>
+
+    <div class="MessageInput">
+
+      <?php
+      if (isset($_GET['ChatRoomID'])) {
+        //generates the url to send the message with
+        $SendMessageTo = "includes/zSendMessage.php?chatRoomID=" . mysqli_real_escape_string($conn, $_GET['ChatRoomID']);
+        echo "<form action='$SendMessageTo'  method='POST'>"
+      ?>
+
+        <input class="messageEntry BorderInputs" type="text" name="messageEntry" placeholder="Type your message here">
+        <button class="messageSend BorderInputs" type="submit" name="messageSend"> Send </button>
+        </form>
+      <?php
+      }
+      ?>
+    </div>
+
+
+  </div>
   </div>
 </body>
